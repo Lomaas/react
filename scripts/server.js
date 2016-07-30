a3f36698a7f2365338dae4cb97d62d3ea5941a98
Simen Johannessen <simenlomaas@gmail.com>
2016-07-30 14:59:18 +0200

    remove /

--- scripts/server.js
+++ scripts/server.js
@@ -3,10 +3,6 @@ var app = express();
 
 app.use(express.static('build'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 app.listen(process.ENV.port || 8080, function () {
   console.log('Example app listening on port 8080!');
 });
