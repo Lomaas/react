5cd75fbe11a6a26ca5af1803ff68d8cfbe64f99c
Simen Johannessen <simenlomaas@gmail.com>
2016-07-30 16:14:54 +0200

    push

--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,10 @@ var app = express();
 var port = process.ENV ? (process.ENV.port || 8080) : 8080;
 app.use(express.static('build'));
 
-app.listen(port, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/', function (req, res) {
+    res.send('its working??');
+});
+
+var server = app.listen(port, function() {
+  console.log('App listening on port %s', server.address().port);
 });
